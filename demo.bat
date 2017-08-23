57305e5379d21a42be5082e6f4c10b08f383b7b7
t-taniai <taniai@iis.u-tokyo.ac.jp>
2017-08-23 23:25:02 +0900

    Update demo.bat

--- demo.bat
+++ demo.bat
@@ -5,6 +5,6 @@ set datasetroot=%~dp0data
 set resultsroot=%~dp0results
 
 mkdir "%resultsroot%"
-"%bin%" -targetDir "%datasetroot%\MiddV2\teddy" -outputDir "%resultsroot%\teddy_dual" -mode MiddV2 -smooth_weight 1 -doDual 1
+"%bin%" -targetDir "%datasetroot%\MiddV2\cones" -outputDir "%resultsroot%\cones" -mode MiddV2 -smooth_weight 1 -doDual 1
 "%bin%" -targetDir "%datasetroot%\MiddV2\teddy" -outputDir "%resultsroot%\teddy" -mode MiddV2 -smooth_weight 1
 "%bin%" -targetDir "%datasetroot%\MiddV3\Adirondack" -outputDir "%resultsroot%\Adirondack" -mode MiddV3 -smooth_weight 0.5
